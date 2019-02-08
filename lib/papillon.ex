6df88f35786c5ff30a890d762d67d44c82a00a05
Calvin Buckley <calvin@cmpct.info>
2019-02-08 14:20:45 -0400

    First attempt at a message passing and concurrent architecture
    Definitely not erlang idiomatic. Also not sure how I'll pass
    messages to the socket handlers - I don't see a spot to do so.
    I guess I could try to move the accept over to the spawned
    process so I can handle :tcp messages by setting active to true
    for the socket, but that seems somewhat clumsy and unsure of how
    not to keep constantly more handlers...
    
    Right now it's not terribly useful, but I think this way the main
    event loop of sorts could dispatch stuff like switchboard or status
    messages to the other NS clients. Likewise, I guess you could use
    something like ETS to store stuff like statuses and Mnesia or Ecto
    to persist lists?

--- lib/papillon.ex
+++ lib/papillon.ex
@@ -2,88 +2,115 @@ defmodule Papillon.NotificationServer do
 	require Logger
 
 	# XXX: Erlang supervision; start new servers with proper linkage
-
-	def accept_ns() do
-		accept_ns(1863) # default msnp port
+	def main() do
+		me = self()
+		listener = spawn(fn -> accept_ns(me) end)
+		recv_loop
+	end
+	
+	defp recv_loop() do
+		receive do
+			{:newClient, clientPid} ->
+				Logger.debug("New client: #{inspect clientPid}")
+				recv_loop()
+			x ->
+				Logger.debug("Huh?")
+				IO.inspect(x)
+				recv_loop()
+		end
 	end
 
-	def accept_ns(port) do
+	def accept_ns(parent) do
+		accept_ns(parent, 1863) # default msnp port
+	end
+
+	def accept_ns(parent, port) do
 		# using magic names per https://elixir-lang.org/getting-started/mix-otp/task-and-gen-tcp.html
 		# i do wonder about the viability of this though
 		{:ok, socket} = :gen_tcp.listen(port, [:binary,
 			packet: :line, # XXX: erlang docs are horrible about what these things mean
 			active: false, reuseaddr: true])
 		Logger.info("NS listening")
-		listen_ns(socket)
+		listen_ns(parent, socket)
 	end
 
 	# Notification server
-	defp listen_ns(socket) do
+	defp listen_ns(parent, socket) do
 		{:ok, client} = :gen_tcp.accept(socket)
-		pid = spawn(fn -> serve_ns(client, %{}) end)
-		listen_ns(socket)
+		pid = spawn(fn -> serve_ns(parent, client, %{}) end)
+		send(parent, {:newClient, pid})
+		listen_ns(parent, socket)
 	end
 
 	# the idea is we can keep the state of the connection in clientState
 	# the serve "loop" will process each command and return a new state
 	# for the next command
 	# hopefully it works - it's functional, i hope
-	defp serve_ns(client, clientState) do
+	defp serve_ns(parent, client, clientState) do
 		# temp debug thing
-		line = client
-			|> read_line()
+		{readStatus, line} = :gen_tcp.recv(client, 0)
+		case readStatus do
+			:ok ->
+				serve_ns_ok(parent, line, client, clientState)
+			_ ->
+				send(parent, {:clientFailed, self()})
+		end
+	end
+
+	defp serve_ns_ok(parent, line, client, clientState) do
 		Logger.debug(line)
 		# let's split this into tokens
 		splitLine = String.split(line)
 		# XXX: This loop probably needs rewriting
-		newState = case splitLine do
+		case splitLine do
 			["VER", id | protocols] ->
 				latestProto = hd(protocols)
 				# XXX: MSNIM 4.6 hangs on SYN if we try to use MSNP7...
 				#write_line(client, "VER #{id} #{latestProto}\r\n")
 				write_line(client, "VER #{id} MSNP6\r\n")
-				clientState
+				serve_ns(parent, client, clientState)
 			["CVR", id | _] ->
 				# Dummy response
 				write_line(client, "CVR #{id} 1.0.0000, 1.0.0000 1.0.0000 about:blank about:blank")
-				clientState
+				serve_ns(parent, client, clientState)
 			["INF", id] ->
 				# We only support MD5 for now
 				write_line(client, "INF #{id} MD5\r\n")
-				clientState
+				serve_ns(parent, client, clientState)
 			["USR", id, "MD5", "I", passport] ->
 				# This would be the point that if we were a
 				# dispatch server, we could transfer to a real
 				# notitifcation server.
 				# XXX: Actually validate and attach salt et al
 				write_line(client, "USR #{id} MD5 S SALT\r\n")
-				Map.merge(clientState, %{:passport => passport})
+				newState = Map.merge(clientState, %{:passport => passport})
+				serve_ns(parent, client, newState)
 			["USR", id, "MD5", "S", pwhash] ->
 				# Happy hour
 				# XXX: Actually validate the salted PW
 				# The 1 at the end means you're a validated account
 				write_line(client, "USR #{id} OK #{Map.get(clientState, :passport)} HelloWorld 1\r\n")
-				clientState
+				serve_ns(parent, client, clientState)
 			["CHG", id, newStatus] ->
 				write_line(client, "CHG #{id} #{newStatus}\r\n") # XXX: escargot w/ 3.6 appends a 0 here?
 				# XXX: Actually keep track of state and send the initial ILNs
-				clientState
+				serve_ns(parent, client, clientState)
 			["LSG", id] ->
 				send_groups(client, id, 0, Map.get(clientState, :passport))
-				clientState
+				serve_ns(parent, client, clientState)
 			["LST", id, "FL"] ->
 				# XXX: better way for generation ID stuff?
 				send_forward_list(client, id, 0, Map.get(clientState, :passport))
-				clientState
+				serve_ns(parent, client, clientState)
 			["LST", id, "RL"] ->
 				send_reverse_list(client, id, 0, Map.get(clientState, :passport))
-				clientState
+				serve_ns(parent, client, clientState)
 			["LST", id, "AL"] ->
 				send_allow_list(client, id, 0, Map.get(clientState, :passport))
-				clientState
+				serve_ns(parent, client, clientState)
 			["LST", id, "BL"] ->
 				send_block_list(client, id, 0, Map.get(clientState, :passport))
-				clientState
+				serve_ns(parent, client, clientState)
 			["SYN", id, genId] ->
 				# We're supposed to diff the changes per generation (or send all for 0)
 				newGenId = elem(Integer.parse(genId), 0) + 1
@@ -97,19 +124,21 @@ defmodule Papillon.NotificationServer do
 				send_reverse_list(client, id, newGenId, Map.get(clientState, :passport))
 				write_line(client, "GTC #{id} #{newGenId} A\r\n")
 				write_line(client, "BLP #{id} #{newGenId} AL\r\n")
-				clientState
+				serve_ns(parent, client, clientState)
 			["PNG"] ->
 				write_line(client, "QNG\r\n")
-				clientState
+				serve_ns(parent, client, clientState)
+			["OUT"] ->
+				write_line(client, "OUT\r\n")
+				send(parent, {:clientDisconnect, self()})
+				:gen_tcp.close(client)
 			[_, id] ->
 				write_line(client, "500 #{id}\r\n")
-				clientState
+				serve_ns(parent, client, clientState)
 			[_, id | _] ->
 				write_line(client, "500 #{id}\r\n")
-				clientState
+				serve_ns(parent, client, clientState)
 		end
-		# nothing is implemented, haha
-		serve_ns(client, newState)
 	end
 
 	# XXX: Actually.... have a list
@@ -134,11 +163,6 @@ defmodule Papillon.NotificationServer do
 
 	defp send_reverse_list(client, seqId, genId, passport) do
 		write_line(client, "LST #{seqId} RL #{genId} 0 0\r\n")
-	end
-
-	defp read_line (client) do
-		{:ok, data} = :gen_tcp.recv(client, 0)
-		data
 	end
 
 	defp write_line(client, line) do
